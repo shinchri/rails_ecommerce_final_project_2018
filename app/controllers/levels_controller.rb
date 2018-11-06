504bfde4496379483aedf56d03b0027bead3e23c
shinchri <shinchri7@gmail.com>
2018-11-06 10:50:07 -0600

    Created controllers w/ index & show views for all models & updated route

---
+++ app/controllers/levels_controller.rb
@@ -0,0 +1,7 @@
+class LevelsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  resources :types, only: [:index, :show]
+  resources :products, only: [:index, :show]
+  resources :orders, only: [:index, :show]
+  resources :order_items, only: [:index, :show]
+  resources :levels, only: [:index, :show]
+  resources :customers, only: [:index, :show]
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
