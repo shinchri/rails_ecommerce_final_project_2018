d6849c2ce2a8c055de3a7815fcf6cf9f28e47618
shinchri <shinchri7@gmail.com>
2018-11-06 20:27:04 -0600

    Added Contact and About page with content change possible w/ admin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
   def about
+    @about_title = Page.find(1).title
+    @about_content = Page.find(1).content
   end
 
   def contact
+    @contact_title = Page.find(2).title
+    @contact_content = Page.find(2).content
   end
 end
