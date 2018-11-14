f4cceb269ef3d945e30ce8db89c2a0d78c3a1627
shinchri <shinchri7@gmail.com>
2018-11-14 11:30:50 -0600

    Installed ImageMagick and added 'mini_magick' gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-  # before_action :load_levels
-  #
-  # private
-  # def load_levels
-  #   @levels = Level.all
-  # end
+  before_action :load_levels
+
+  private
+  def load_levels
+    @levels_for_nav = Level.all
+  end
 end
