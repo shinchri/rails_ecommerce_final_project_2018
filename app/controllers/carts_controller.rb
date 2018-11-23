ff8ccd8bce7fb05f591ccdd40462d83c3503af73
shinchri <shinchri7@gmail.com>
2018-11-23 16:31:23 -0600

    Added button to clear the cart session array

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,10 @@
 class CartsController < ApplicationController
   def index
   end
+
+  def clear_shopping_cart_lists
+    session[:product_id] = []
+    session[:quantity] = []
+    redirect_to action: 'index'
+  end
 end
