561e7e68a777e9eea891235ce1d16114118bf2a7
shinchri <shinchri7@gmail.com>
2018-11-22 23:37:37 -0600

    Allow navigation by categories; fixed the stylings of the categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :load_levels
 
   private
-
   def load_levels
     @levels_for_nav = Level.all
+    @types_for_nav = Type.all
   end
 end
