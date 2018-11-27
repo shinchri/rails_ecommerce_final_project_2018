f6bbe785d794aef92d54450de73619b45bfd4d3a
shinchri <shinchri7@gmail.com>
2018-11-26 19:43:37 -0600

    Generated charges controller with new and create; changed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :charges, only: %i[new create]
   resources :carts, only: %i[index] do
     collection do
       post :checkout
